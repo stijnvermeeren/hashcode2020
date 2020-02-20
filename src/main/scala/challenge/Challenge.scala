f849bd03eb6e65da7c4405e0114058a743fd4361
Nicu Reut <nicu.reut@starmind.com>
2020-02-20 21:05:57 +0100

    Try divide solution

--- src/main/scala/challenge/Challenge.scala
+++ src/main/scala/challenge/Challenge.scala
@@ -1,6 +1,5 @@
 package challenge
 
-import scala.annotation.tailrec
 import scala.collection.parallel.CollectionConverters._
 
 object Challenge extends App {
@@ -25,15 +24,23 @@ object Challenge extends App {
   }
 
 
-  @tailrec
   def calculateSelection(days: Int, libs: Seq[Library], allBooks: Int, res: Seq[MaxScoreLibrary] = Seq.empty, usedBooks: Set[Int] = Set.empty): Seq[MaxScoreLibrary] = {
     if (libs.isEmpty || usedBooks.size == allBooks) {
       res
     } else {
       val topLibraries = libs.par.map(Scoring.maxScorePerLibrary(days, _, usedBooks)).seq.sortBy(library => library.score).reverse
       val bestLibrary = topLibraries.head
-      val bestLibraryInitial = libs.par.find(_.id == bestLibrary.id).get
-      calculateSelection(days - bestLibraryInitial.singUpTime, libs.par.filterNot(_.id == bestLibrary.id).seq, allBooks, res :+ bestLibrary, usedBooks ++ bestLibrary.books.toSet)
+      val bestSelectionForSignUpPeriod = calculateSelection(bestLibrary.library.singUpTime, libs, allBooks)
+
+      val bestSelection = if (bestSelectionForSignUpPeriod.total >= bestLibrary.score)
+        bestSelectionForSignUpPeriod
+      else Seq(bestLibrary)
+      calculateSelection(days - bestLibrary.library.singUpTime, libs.filterNot(bestSelection.contains), allBooks, res ++ bestSelection, usedBooks ++ bestSelection.flatMap(_.books).toSet)
     }
   }
+
+  implicit class SelectionImpro(selection: Seq[MaxScoreLibrary]) {
+    def total: Int = selection.map(_.score).sum
+  }
+
 }
--- src/main/scala/challenge/Scoring.scala
+++ src/main/scala/challenge/Scoring.scala
@@ -10,13 +10,13 @@ object Scoring {
   def maxScorePerLibrary(daysLeft: Int, library: Library, usedBooks: Set[Int]): MaxScoreLibrary = {
     val scanningDays = daysLeft - library.singUpTime
     if (scanningDays <= 0) {
-      MaxScoreLibrary(library.id, 0, Seq.empty)
+      MaxScoreLibrary(library.id, 0, Seq.empty, library)
     } else {
       val booksToBeScanned = scanningDays * library.booksPerDay
       val booksToScan = mostValuableBookIds(library).par.filterNot(book => usedBooks.contains(book.id)).take(booksToBeScanned)
-      MaxScoreLibrary(library.id, booksToScan.map(_.score).sum, booksToScan.map(_.id).seq)
+      MaxScoreLibrary(library.id, booksToScan.map(_.score).sum, booksToScan.map(_.id).seq, library)
     }
   }
 }
 
-case class MaxScoreLibrary(id: Int, score: Int, books: Seq[Int])
+case class MaxScoreLibrary(id: Int, score: Int, books: Seq[Int], library: Library)
