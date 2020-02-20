63ab9532abea96804800f80f11746d523f945d62
Nicu Reut <nicu.reut@starmind.com>
2020-02-20 19:18:56 +0100

    Update problem writing to include library id

--- src/main/scala/challenge/Solution.scala
+++ src/main/scala/challenge/Solution.scala
@@ -7,14 +7,18 @@ case class Solution(librarySelections: Seq[LibrarySelection]) {
   def writeToFile(path: String): Unit = {
     val pw = new PrintWriter(new File(path))
     pw.write(s"${librarySelections.size}\n")
-    for (LibrarySelection(id, bookIds) <- librarySelections) {
-      pw.write(s"$id ${bookIds.size}\n")
-      pw.write(s"${bookIds.mkString(" ")}\n")
+    for (LibrarySelection(id, books) <- librarySelections) {
+      pw.write(s"$id ${books.size}\n")
+      pw.write(s"${books.map(_.bookId).mkString(" ")}\n")
     }
     pw.close()
   }
 
-  def score(problem: ProblemData): Int = ???
+  def score(): Int = {
+    librarySelections.flatMap(_.scannedBooks.map(_.score)).sum
+  }
 }
 
-case class LibrarySelection(libraryId: Int, bookIds: Seq[Int])
+case class LibrarySelection(libraryId: Int, scannedBooks: Seq[ScannedBook])
+
+case class ScannedBook(bookId: Int, score: Int)
